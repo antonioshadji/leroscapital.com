5dd3b431bb0269548acea3af83f1effde08b3eec
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-05-30 19:40:05 -0400

    added gopher4 sample to /test

--- app.go
+++ app.go
@@ -1,41 +1,127 @@
 package main
 
 import (
-  "html/template"
-  "net/http"
-  "google.golang.org/appengine"
-//  "google.golang.org/appengine/datastore"
-//  "google.golang.org/appengine/log"
+	"fmt"
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
+	"google.golang.org/appengine/log"
+	"html/template"
+	"net/http"
+	"time"
 )
 
-type Todo struct {
-  Title string
-  Done  bool
+var (
+	tmpl = template.Must(template.ParseGlob("templates/*"))
+)
+
+type PageDetails struct {
+	PageTitle string
+	Posted    time.Time
 }
 
-type TodoPageData struct {
-  PageTitle string
-  Todos     []Todo
+type Post struct {
+	Author  string
+	Message string
+	Posted  time.Time
 }
 
-func Server(w http.ResponseWriter, r *http.Request) {
+type templateParams struct {
+	Notice  string
+	Name    string
+	Message string
+	Posts   []Post
+}
 
-  data := TodoPageData{
-    PageTitle: "Leros Capital",
-    Todos: []Todo{
-      {Title: "Task 1", Done: false},
-      {Title: "Task 2", Done: true},
-      {Title: "Task 3", Done: true},
-    },
-  }
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-  tmpl := template.Must(template.ParseGlob("templates/*"))
-  tmpl.ExecuteTemplate(w, "home", data)
+	data := PageDetails{
+		PageTitle: "Leros Capital",
+	}
+
+	err := tmpl.ExecuteTemplate(w, "home", data)
+	if err != nil {
+
+	}
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Sprintf(r.URL.Path)
+	if r.URL.Path != "/test" {
+		http.Redirect(w, r, "/test", http.StatusFound)
+		return
+	}
+	ctx := appengine.NewContext(r)
+	params := templateParams{}
+	q := datastore.NewQuery("Post").Order("-Posted").Limit(20)
+
+	if _, err := q.GetAll(ctx, &params.Posts); err != nil {
+		log.Errorf(ctx, "Getting posts: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't get latest posts. Refresh?"
+		err := tmpl.ExecuteTemplate(w, "test", params)
+		if err != nil {
+			log.Errorf(ctx, "execute template: ", err)
+		}
+		return
+	}
+
+	if r.Method == "GET" {
+		err := tmpl.ExecuteTemplate(w, "test", params)
+		if err != nil {
+			log.Errorf(ctx, "execute template: ", err)
+		}
+		return
+	}
+
+	// It's a POST request, so handle the form submission.
+	post := Post{
+		Author:  r.FormValue("name"),
+		Message: r.FormValue("message"),
+		Posted:  time.Now(),
+	}
+
+	if post.Author == "" {
+		post.Author = "Anonymous Gopher"
+	}
+	params.Name = post.Author
+
+	if post.Message == "" {
+		w.WriteHeader(http.StatusBadRequest)
+		params.Notice = "No message provided"
+		err := tmpl.ExecuteTemplate(w, "test", params)
+		if err != nil {
+			log.Errorf(ctx, "execute template: ", err)
+		}
+		return
+	}
+
+	key := datastore.NewIncompleteKey(ctx, "Post", nil)
+
+	if _, err := datastore.Put(ctx, key, &post); err != nil {
+		log.Errorf(ctx, "datastore.Put: %v", err)
+
+		w.WriteHeader(http.StatusInternalServerError)
+		params.Notice = "Couldn't add new post. Try again?"
+		params.Message = post.Message // Preserve their message so they can try again.
+		err := tmpl.ExecuteTemplate(w, "test", params)
+		if err != nil {
+			log.Errorf(ctx, "execute template: ", err)
+		}
+		return
+	}
+
+	// Prepend the post that was just added.
+	params.Posts = append([]Post{post}, params.Posts...)
+
+	params.Notice = fmt.Sprintf("Thank you for your submission, %s!", post.Author)
+	err := tmpl.ExecuteTemplate(w, "test", params)
+	if err != nil {
+		log.Errorf(ctx, "execute template: ", err)
+	}
 }
 
 func main() {
-
-  http.HandleFunc("/", Server)
-  appengine.Main()
-  // log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/test", testHandler)
+	http.HandleFunc("/", homeHandler)
+	appengine.Main()
 }
