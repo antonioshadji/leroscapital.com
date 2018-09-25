da3e267a4bcfa1120fd917d1850fdc078adbddf2
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-09-25 09:31:06 -0400

    show logged in user

--- js/ui.js
+++ js/ui.js
@@ -7,6 +7,7 @@ function onSignIn (googleUser) {
   console.log('Name: ' + profile.getName())
   console.log('Image URL: ' + profile.getImageUrl())
   console.log('Email: ' + profile.getEmail()) // This is null if the 'email' scope is not present.
+  document.getElementById('t_loggedin_user').innerHTML = profile.getName()
   document.getElementById('b_logout').classList.remove('d-none')
 }
 
@@ -25,6 +26,7 @@ window.addEventListener('load', (event) => {
   document.getElementById('b_logout').addEventListener('click', () => {
     console.log('click #b_login')
     document.getElementById('b_logout').classList.add('d-none')
+    document.getElementById('t_loggedin_user').innerHTML = ''
     signOut()
   })
 })
