6dd633281d7ecd4f0fdd120d56d41c205b74304d
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-10-24 09:52:07 -0400

    add image to login header

--- js/ui.js
+++ js/ui.js
@@ -5,9 +5,9 @@ function onSignIn (googleUser) {
   var profile = googleUser.getBasicProfile()
   console.log('ID: ' + profile.getId()) // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName())
-  console.log('Image URL: ' + profile.getImageUrl())
   console.log('Email: ' + profile.getEmail()) // This is null if the 'email' scope is not present.
-  document.getElementById('t_loggedin_user').innerHTML = profile.getName()
+  document.getElementById('t_loggedin_user').innerHTML =
+  `${profile.getName()} <img class='rounded-circle' src='${profile.getImageUrl()}' height='36px'/>`
   document.getElementById('b_logout').classList.remove('d-none')
 }
 
@@ -15,6 +15,8 @@ function signOut () {
   var auth2 = gapi.auth2.getAuthInstance()
   auth2.signOut().then(function () {
     console.log('User signed out.')
+    document.getElementById('b_logout').classList.add('d-none')
+    document.getElementById('t_loggedin_user').innerHTML = ''
   })
 }
 
@@ -24,9 +26,7 @@ window.addEventListener('load', (event) => {
 
   /* project specific */
   document.getElementById('b_logout').addEventListener('click', () => {
-    console.log('click #b_login')
-    document.getElementById('b_logout').classList.add('d-none')
-    document.getElementById('t_loggedin_user').innerHTML = ''
+    console.log('click #b_logout')
     signOut()
   })
 })
