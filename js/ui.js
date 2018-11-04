97cfe396957cd494c9fb173cc1b96b1ce8ee2950
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2018-11-04 15:34:41 -0500

    get token for back end

--- js/ui.js
+++ js/ui.js
@@ -1,11 +1,14 @@
 /* global gapi */
-/* eslint-disable no-unused-vars */
+/* eslint-disable no-unused-vars, camelcase */
 
 function onSignIn (googleUser) {
   var profile = googleUser.getBasicProfile()
   console.log('ID: ' + profile.getId()) // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName())
   console.log('Email: ' + profile.getEmail()) // This is null if the 'email' scope is not present.
+  var id_token = googleUser.getAuthResponse().id_token
+  console.log('ID Token for backend: ' + id_token)
+
   document.getElementById('t_loggedin_user').innerHTML =
   `${profile.getName()} <img class='rounded-circle' src='${profile.getImageUrl()}' height='36px'/>`
   document.getElementById('b_logout').classList.remove('d-none')
