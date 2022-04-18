87c862443c702b7162d7a683934b4431752ccf68
Antonios Hadjigeorgalis <Antonios@Hadji.co>
2022-04-18 19:50:13 -0400

    revert to working map example with markers added

---
+++ README.md
@@ -0,0 +1,1 @@
+2022-04-18 19:49:19 added secret manager accessor permissions to project service account
--- js/index.js
+++ js/index.js
@@ -1,18 +1,32 @@
 (() => {
   "use strict";
-
+  var e = {
+      d: (o, t) => {
+        for (var r in t)
+          e.o(t, r) &&
+            !e.o(o, r) &&
+            Object.defineProperty(o, r, { enumerable: !0, get: t[r] });
+      },
+      o: (e, o) => Object.prototype.hasOwnProperty.call(e, o),
+      r: (e) => {
+        "undefined" != typeof Symbol &&
+          Symbol.toStringTag &&
+          Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
+          Object.defineProperty(e, "__esModule", { value: !0 });
+      },
+    },
+    o = {};
+  e.r(o), e.d(o, { initMap: () => r });
   const t = {
     sis: { center: { lat: 38.8712041, lng: -77.1143355 } },
     antonios: { center: { lat: 40.0005234, lng: -75.2943201 } },
   };
-
   function r() {
     const e = new google.maps.Map(document.getElementById("map"), {
       zoom: 9,
       center: { lat: 39.2903848, lng: -76.6121893 },
       mapTypeId: "terrain",
     });
-
     for (const o in t) {
       new google.maps.Circle({
         strokeColor: "#FF0000",
@@ -24,12 +38,14 @@
         center: t[o].center,
         radius: 1e5, // 100km
       });
+
       new google.maps.Marker({
         position: t[o].center,
         map: e,
       });
     }
   }
-
-  window.initMap = initMap;
+  var n = window;
+  for (var l in o) n[l] = o[l];
+  o.__esModule && Object.defineProperty(n, "__esModule", { value: !0 });
 })();
